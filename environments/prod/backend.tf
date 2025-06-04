f28e502a381b8a4b8255f833899e0c5f662ed911
Yachitha <yachithasandaruwan@gmail.com>
2025-06-04 23:01:12 +0200

    reconfigured the terraform state management

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bistrobyte-tf-state"
+    bucket         = "my-unique-tf-state"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "bistrobyte-tf-lock"
