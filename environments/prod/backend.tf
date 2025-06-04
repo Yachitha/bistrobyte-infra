a3102f733a177805382be5f20278099e1cea77bc
Yachitha Sandaruwan <yachithasandaruwan@gmail.com>
2025-06-04 21:47:26 +0200

    Update default AWS region to eu-north-1

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bistrobyte-tf-state"
     key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     dynamodb_table = "bistrobyte-tf-lock"
   }
 }
